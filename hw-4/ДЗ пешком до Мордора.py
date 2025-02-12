08fb767dbe98270685a249fd9b3ca673d04b41ee
Ibolvachevamailu <ibolvacheva@mail.ru>
2025-02-12 23:54:05 +0300

    Add files via upload

--- hw-4/ДЗ пешком до Мордора.py
+++ hw-4/ДЗ пешком до Мордора.py
@@ -1,23 +1,20 @@
-import lessons as np
+import numpy as np
 
-def cal_euclidean(a: int, b: int):
-    i = 10
-    a = int(a)
-    b = int(b)
-    for i in range(a, b):
-        distance = ((a - b) ** 2) ** 0.5
-        i += 10
+def cal_euclidean(array_a: np.ndarray, array_b: np.ndarray):
+        distance = np.sqrt(np.sum((a - b) ** 2))
+        return distance
+
+
+def cal_manhattan(array_a: np.ndarray, array_b: np.ndarray):
+    distance = np.sum(np.abs(a)-np.abs(b))
+    return distance
+#
+def cal_cosine(array_a: np.ndarray, array_b: np.ndarray):
+    distance = np.dot(a, b)/(np.linalg.norm(a) * np.linalg.norm(b))
     return distance
 
-# def cal_manhattan(a, b):
-#     ## Your code here
-#     return distance
-#
-# def cal_cosine(a, b):
-#     ## Your code here
-#     return distance
 a = np.random.randint(-10, 10, size=10)
 b = np.random.randint(-10, 10, size=10)
 print(cal_euclidean(a, b))
-# print(cal_manhattan(a, b))
-# print(cal_cosine(a, b))
+print(cal_manhattan(a, b))
+print(cal_cosine(a, b))
