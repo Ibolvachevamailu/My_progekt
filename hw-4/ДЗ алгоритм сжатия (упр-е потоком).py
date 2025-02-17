dfbbeff47204e5ef10c7795ac6bfbfe5b9e302d3
Ibolvachevamailu <ibolvacheva@mail.ru>
2025-02-17 22:05:10 +0300

    Update ДЗ алгоритм сжатия (упр-е потоком).py

--- hw-4/ДЗ алгоритм сжатия (упр-е потоком).py
+++ hw-4/ДЗ алгоритм сжатия (упр-е потоком).py
@@ -5,18 +5,19 @@
 # aaabbbbccccc → 3a4b5cНаписать упрощенную версию алгоритма сжатия RLE. Алгоритм RLE объединяет подряд
 
 string_1 = input('введите строку ')
-count = 1
-result = []
-
-
-for i in range(1, len(string_1)):
-    if string_1[i] == string_1[i - 1]:
-        count += 1
-    else:
-        result.append(f'{count}{string_1[i - 1]}')
-        count = 1
-result.append(f'{count}{string_1[-1]}')
-print(''.join(result))
+if not string_1:
+    print("введена пустая строка")
+else:
+    count = 1
+    result = []
+    for i in range(1, len(string_1)):
+        if string_1[i] == string_1[i - 1]:
+            count += 1
+        else:
+            result.append(f'{count}{string_1[i - 1]}')
+            count = 1
+    result.append(f'{count}{string_1[-1]}')
+    print(''.join(result))
 
 
 
